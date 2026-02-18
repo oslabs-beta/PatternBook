7625cd9a2933628ca7506922e82ed44dcc060dff
Trish OMeara Gillespie <trishedith@gmail.com>
2026-02-18 17:24:52 -0500

    feat- inprogress

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,14 +4,14 @@ import Dashboard from "./dashboard-etc/Dashboard.tsx";
 
 function App() {
   // Load manifest into store on mount
-  // const setManifest = useRegistryStore((state) => state.setManifest);
+  const setManifest = useRegistryStore((state) => state.setManifest);
 
-  // useEffect(() => {
-  //   fetch("/mock-data/manifest.json")
-  //     .then((res) => res.json())
-  //     .then((data) => setManifest(data))
-  //     .catch(console.error);
-  // }, [setManifest]);
+  useEffect(() => {
+    fetch("/mock-data/manifest.json")
+      .then((res) => res.json())
+      .then((data) => setManifest(data))
+      .catch(console.error);
+  }, [setManifest]);
 
   return (
     <>
