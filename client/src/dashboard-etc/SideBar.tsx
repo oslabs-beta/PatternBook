82a421c93f913cb847da9fec257b7a3be74122dc
Trish OMeara Gillespie <trishedith@gmail.com>
2026-02-18 22:09:25 -0500

    save before merge

--- client/src/dashboard-etc/SideBar.tsx
+++ client/src/dashboard-etc/SideBar.tsx
@@ -1,22 +1,29 @@
 import SearchBar from "./SearchBar.tsx";
 import { Play, Search, GalleryThumbnails, Bell, Rewind } from "lucide-react";
 
+import PlayGround from "./PlayGround.tsx";
 import { useState } from "react";
 import "../main.css";
+import { useManifest } from "../hooks/useManifest.ts";
 
 function SideBar() { 
   const [isVisible, setIsVisible] = useState(false);
+  const handleManifest = useManifest;
+
   return (
   <div className="flex-row justify-self-start text-blue-500 border border-blue-500 w-[15%]">
       <h6> {"Sidebar"} </h6>
         <Play 
-          onClick={() => setIsVisible(!isVisible) }
+          onClick={() => handleManifest}
           className="cursor-pointer hover:text-red-500 transition"
           size={24}
         />
+
+
         {isVisible && (
           <div className="flex-row justify-center text-purple-400 border border-purple-500 w-[90%]">
-            {"Display Playground/ Documentation"}
+            {"handleManifest"}
+            <PlayGround />
           </div>
         )}
       
