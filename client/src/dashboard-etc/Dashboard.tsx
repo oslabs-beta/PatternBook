aa3c633b4d50879ae35ab1f6b332471e4037882c
Trish O'Meara - Gillespie <35871593+trishedith@users.noreply.github.com>
2026-02-18 21:45:52 -0500

    Feat: sidebar menu (#10)
    * feat: sidebar in-progress
    
    * feat- inprogress
    
    * feat: wireframe for sidebar

--- client/src/dashboard-etc/Dashboard.tsx
+++ client/src/dashboard-etc/Dashboard.tsx
@@ -1,28 +1,17 @@
 import "../main.css";
-import SearchBar from "./SearchBar";
 import PlayGround from "./PlayGround";
 import SideBar from "./SideBar";
-import { useState } from "react";
-import { Search } from "lucide-react";
+
+
 
 function Dashboard() {
-  const [isVisible, setIsVisible] = useState(false);
+
+  
   return (
     <>
-      <div className="flex-column align-start">
+      <div className="flex column">
         <SideBar />
         <PlayGround />
-        <Search
-          onClick={() => setIsVisible(isVisible)}
-          className="cursor-pointer hover:text-blue-100 transition"
-          size={24}
-        />
-        {isVisible && (
-          <div>
-            <SearchBar />
-          </div>
-        )}
-        {/* <Search onClick={() => setShowComponent(true)} /> */}
       </div>
     </>
   );
--- client/src/dashboard-etc/PlayGround.tsx
+++ client/src/dashboard-etc/PlayGround.tsx
@@ -1,10 +1,12 @@
 // I'm labelling this as Playground.  Probably will be static for the MVP but just figured this was clear
 import "../main.css";
+
+
 function PlayGround() {
   return (
     <>
-      <div className="justify-center text-blue-500 width-400px border border-color-blue-400">
-        <p> playground / view documentaion </p>
+      <div className="flex justify-center text-blue-500 width-400px border border-color-blue-400 w-[75%]">
+        <p className="justify-center"> playground/ documentation</p>
       </div>
     </>
   );
